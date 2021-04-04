ea0c02d35eaa61b14e9dea917bea4fcee86906f7
Nicolás Acuña <80644036+nicoacuna10@users.noreply.github.com>
2021-04-04 09:09:13 -0400

    Update eliminarBomberos.c

--- eliminarBomberos.c
+++ eliminarBomberos.c
@@ -3,26 +3,39 @@
 #include <stdbool.h>
 #include "list.h"
 
+typedef struct{
+  char* rut;
+  char* nombre;
+  int disponibilidad[7];
+}Bombero;
 
-void eliminarBomberos(char *rut, List *list, unsigned long long *talla){
-  printf("Ingrese rut del bombero a despedir: ");
-  scanf("%[^\n]s", rut);
-  getchar();
-    int j, i;
+void eliminarBomberos(char *rut, List *listaBomberos, unsigned long long *talla){
+    printf("Ingrese rut del bombero a despedir: ");
+    scanf("%[^\n]s", rut);
+    getchar();
+    int i;
     bool encontrado;
-    for( j = 0; j < *talla; j++){
-        encontrado = existeRutBombero(list, rut);
+    Bombero* aux = firstList(listaBomberos);
+    while(aux!=NULL){
+        i=0;
+        encontrado = true;
+        aux->rut = (char*)aux->rut;
+        for( i = 0; aux->rut[i]; i++){
+            if(aux->rut[i] != rut[i]){
+                encontrado = false;
+            }
+        }
         if(encontrado){
-            popCurrent(list);
+            aux = popCurrent(listaBomberos);
             *talla = *talla-1;
-            vector = realloc (vector, (*talla) * sizeof(Bombero));
             printf("-----------------------\n");
             printf("BOMBERO ELIMINADO\n");
             printf("-----------------------\n");
             return;
         }
+        aux = nextList(listaBomberos);
     }
     if(!encontrado){
-        printf("NO EXISTE BOMBERO\n");
+        printf("NO EXISTE REGISTRO\n");
     }
 }
